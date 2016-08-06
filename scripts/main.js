160b0036e01123daeba999af2b328c214cb55e4a
Santiago Lizardo <santiago.lizardo@gmail.com>
2016-08-06 22:58:12 +0100

    Simplifying the homepage. Links to the stores instead of keeping individual game pages here.

--- scripts/main.js
+++ scripts/main.js
@@ -14,18 +14,5 @@ $( document ).ready( function() {
 		pagination: true,
 		updateURL: false,
 	});
-
-
-	$('.app-icons li').on( "over mouseover", function() {
-		$('.gameInfo h1').text( $( this ).data( 'name' ) );
-		$('.mobile .gameWindow').removeClass().addClass( 'gameWindow '+$( this ).attr('class'));
-		$('.mobile .gameWindow').css('background-image', 'url(images/games/'+$( this ).data( 'name' )+'_captures.png)' );
-		$( '#platformValue' ).html( $( this ).data( 'platform' ) );
-	});
-
-	$('.app-icons li').on( "click", function() {
-		$('.app-icons li').removeClass( 'active');
-		$( this ).addClass( 'active');
-	});
 });
 
